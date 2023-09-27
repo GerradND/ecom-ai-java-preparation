3dc3db7cdeeceaa88ef5c9a143e57f6ca8ed2345
GerradND <gerrad1106@gmail.com>
2023-09-27 08:34:39 +0700

    Delete unused method

--- src/main/java/com/example/onboarding/testDay1/dao/CarsDao.java
+++ src/main/java/com/example/onboarding/testDay1/dao/CarsDao.java
@@ -8,6 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.onboarding.testDay1.model.CarsModel;
 
 public interface CarsDao extends JpaRepository<CarsModel, String> {
-	List<CarsModel> findByName(String name);
 	List<CarsModel> findByRental_Id(long id);
 }
